d5536b73a19d4c31839b2d7481d82f82e2ab8510
user <abhay_pratap@thbs.com>
2021-02-09 23:33:39 +0000

    Added example

---
+++ gladyshev.pro/README.md
@@ -0,0 +1,7 @@
+# gladyshev.pro
+Personal website powered by Vue.js and served via S3 using Serverless framework
+
+## Deployment
+
+`serverless client deploy`
+
---
+++ gladyshev.pro/client/src/components/Index.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="hello">
+    <img src="./../assets/logo.png" />
+    <h1>Mitya Gladyshev</h1>
+    <h2>Engineering Manager & Digital Nomad</h2>
+    <ul>
+      <li>
+        <a href="https://www.linkedin.com/in/dgladyshev/" target="_blank">
+          linkedin
+        </a>
+      </li>
+      <li>
+        <a href="https://www.facebook.com/nomad.mitya" target="_blank">
+          facebook
+        </a>
+      </li>
+      <li>
+        <a href="https://www.instagram.com/mitya_gladyshev" target="_blank">
+          instagram
+        </a>
+      </li>
+      <li>
+        <a href="https://github.com/dgladyshev" target="_blank">
+          github
+        </a>
+      </li>
+      <br />
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: normal;
+  margin: 10px;
+}
+h2 {
+  font-weight: normal;
+  margin: 10px;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0px 7px 0px auto;
+}
+
+a {
+  color: #1987b9;
+  font-weight: lighter;
+}
+</style>
