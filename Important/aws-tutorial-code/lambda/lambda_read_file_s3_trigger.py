92a17246282cc57877ef46046ad60ae50f7cc47c
abhay <abhaypratap3536@gmail.com>
2021-03-25 07:33:22 +0000

    Added Examples

---
+++ Important/aws-tutorial-code/lambda/lambda_read_file_s3_trigger.py
@@ -0,0 +1,23 @@
+"""
+-*- coding: utf-8 -*-
+========================
+AWS Lambda
+========================
+Contributor: Chirag Rathod (Srce Cde)
+========================
+"""
+import boto3
+
+
+def lambda_handler(event, context):
+    """Read file from s3 on trigger."""
+    s3 = boto3.client("s3")
+    if event:
+        file_obj = event["Records"][0]
+        bucketname = str(file_obj['s3']['bucket']['name'])
+        filename = str(file_obj['s3']['object']['key'])
+        print("Filename: ", filename)
+        fileObj = s3.get_object(Bucket=bucketname, Key=filename)
+        file_content = fileObj["Body"].read().decode('utf-8')
+        print(file_content)
+    return 'Thanks'
